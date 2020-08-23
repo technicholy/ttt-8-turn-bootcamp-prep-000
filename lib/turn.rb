db190bd2c60ca6bee7ac66d28faaaeea3b15552a
Clayton Pierce <bamer78@gmail.com>
2020-08-23 23:53:28 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,7 +15,20 @@ end
 def input_to_index(user_input)
   user_input.to_i - 1
 end
-
 def move(board, index, current_player = "X")
   board[index] = current_player
+end
+def turn(board)
+  answer = 0
+  def get_answer
+    puts "Please enter 1-9:"
+    pmove = gets.chomp
+    return input_to_index(pmove)
+  end    
+  until answer.between?(1,9)
+    answer = get_answer()
+  end
+  if valid_move?(board, answer
+    display_board(move(board, answer))
+  end
 end
