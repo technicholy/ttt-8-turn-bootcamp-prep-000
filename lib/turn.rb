9c63991b19f964d579945f25a1efaa2762d2817c
Clayton Pierce <bamer78@gmail.com>
2020-08-23 23:37:04 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,7 @@
+def position_taken?(board, index)
+  (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
+    false : true
+end
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
